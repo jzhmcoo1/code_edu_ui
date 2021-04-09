af7bdcc75ee1ed8b9a55e8e00b5da31f8632210f
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-09 21:29:18 +0800

    封装axios

---
+++ utils/request.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import cookie from 'js-cookie'
+
+const service = axios.create({
+  baseURL: process.env.baseURL,
+  timeout: 3000
+})
+
+
+service.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    if (response.data.success) {
+      console.log("响应拦截器---返回数据成功");
+      return response.data
+    }
+  },
+  function (error) {
+    console.log("响应拦截器---返回数据失败");
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+
+export default service

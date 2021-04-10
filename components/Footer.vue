429324d86319ca09a830422d6e27d1217ee3dd3e
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-10 20:55:15 +0800

    静态网页底部

---
+++ components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card>
+    <v-footer v-bind="localAttrs" :padless="padless">
+      <v-card flat tile width="100%" class="primary lighten-1 text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-home", "mdi-email", "mdi-calendar", "mdi-delete"],
+    items: ["default", "absolute", "fixed"],
+    padless: true,
+    variant: "default",
+  }),
+  computed: {
+    localAttrs() {
+      const attrs = {};
+
+      if (this.variant === "default") {
+        attrs.absolute = false;
+        attrs.fixed = false;
+      } else {
+        attrs[this.variant] = true;
+      }
+      return attrs;
+    },
+  },
+};
+</script>

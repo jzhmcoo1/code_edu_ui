7bb6f8317da6387d7d02f31b1c5b604cf1151a62
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-11 23:51:32 +0800

    重新封装axios

--- utils/request.ts
+++ utils/request.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import cookie from 'js-cookie'
-
 const service = axios.create({
   baseURL: process.env.baseURL,
   timeout: 3000
@@ -11,9 +10,12 @@ service.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    console.log(response.data)
     if (response.data.success) {
       console.log("响应拦截器---返回数据成功");
-      return response.data.data
+      return response.data
+    } else {
+      console.log("请求错误")
     }
   },
   function (error) {

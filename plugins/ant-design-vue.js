970c7e14408a1d143ec254d231229568ac2a6458
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-12 00:14:07 +0800

    配置message

--- plugins/ant-design-vue.js
+++ plugins/ant-design-vue.js
@@ -1,4 +1,9 @@
 import Vue from 'vue';
 import { message } from 'ant-design-vue';
+message.config({
+  top: `100px`,
+  duration: 2,
+  maxCount: 3,
+});
 Vue.config.productionTip = false;
 Vue.prototype.$message = message;

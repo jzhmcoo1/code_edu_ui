ac1624013c4cb1e20ca4f6c7986264a19130e150
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-11 19:35:01 +0800

    新增注册/登录的布局

---
+++ layouts/sign.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="sign">
+    <Navbar />
+    <v-main>
+      <v-container class="fill-height justify-center">
+        <v-row class="justify-center">
+          <v-col class="col-sm-8 col-lg-11 col-xl-7">
+            <v-card elevation="4">
+              <v-sheet>
+                <v-row>
+                  <!-- 左边 -->
+                  <v-col
+                    class="info d-none d-md-flex align-center justify-center col-lg-7"
+                  >
+                    <v-icon size="500">laptop</v-icon>
+                  </v-col>
+                  <!-- 右边 -->
+                  <v-col class="d-flex flex-column col-lg-5 px-10 pb-5">
+                    <div>
+                      <v-tabs centered>
+                        <v-tab router to="/login"> 登录 </v-tab>
+                        <v-tab router to="/register"> 注册 </v-tab>
+                      </v-tabs>
+                    </div>
+
+                    <!-- //TODO: 插入登录注册表格 -->
+                    <nuxt />
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "@/components/Navbar.vue";
+export default Vue.extend({
+  components: {
+    Navbar,
+  },
+});
+</script>
+
+<style>
+</style>

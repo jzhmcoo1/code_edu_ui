2dcbc8691463830c2950a74a608ae52b147b9887
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-15 17:03:42 +0800

    新增课程页面

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -1,16 +1,9 @@
-<template>
-  <v-container grid-list-xs>
-    <FloatBtn />
-  </v-container>
-</template>
+<template></template>
 <script lang="ts">
 import Vue from "vue";
-import FloatBtn from "@/components/FloatBtn.vue";
 export default Vue.extend({
-  components: { FloatBtn },
   data() {
     return {};
   },
-  methods: {},
 });
 </script>

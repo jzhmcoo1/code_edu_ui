d1f78f87499de0e99bb619d77cb56f00ca97ad2b
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-15 17:03:28 +0800

    新增文章页面

---
+++ pages/article/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container grid-list-xs>
+    <FloatBtn />
+  </v-container>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import FloatBtn from "@/components/FloatBtn.vue";
+export default Vue.extend({
+  components: { FloatBtn },
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>

851e639efce5c6c521ed97ca1c9b42ddfa213b93
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-22 00:27:44 +0800

    新增个人中心布局

---
+++ layouts/ucenter.vue
@@ -0,0 +1,156 @@
+<template>
+  <!-- 用户中心布局 -->
+  <v-app id="ucenter">
+    <v-navigation-drawer v-model="drawer" app>
+      <!-- 用户中心的drawer -->
+      <!-- 用户信息 -->
+      <v-list>
+        <client-only>
+          <v-list-item class="d-flex justify-center">
+            <v-list-item-avatar size="100">
+              <v-img :src="loginInfo.avatar"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+        </client-only>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              {{ loginInfo.nickname }}
+            </v-list-item-title>
+            <v-list-item-subtitle>{{ loginInfo.mobile }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <!-- 功能栏 -->
+      <v-list nav>
+        <v-list-group
+          v-for="(navItem, index) in nav"
+          :key="index"
+          :prepend-icon="navItem.icon"
+          no-action
+          active-class="info--text"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>{{ navItem.title }}</v-list-item-title>
+          </template>
+          <v-list-item
+            v-for="(child, index) in navItem.child"
+            :key="index"
+            router
+            :to="child.route"
+            exact
+            active-class="info--text"
+          >
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+            <v-list-item-icon>
+              <v-icon v-text="child.icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="font-weight-bold">个人中心</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- 明暗切换器 -->
+      <v-switch
+        class="center"
+        :prepend-icon="$vuetify.theme.dark ? 'nightlight_round' : 'wb_sunny'"
+        inset
+        hide-details
+        v-model="$vuetify.theme.dark"
+      ></v-switch>
+      <v-btn text router to="/" icon>
+        <v-icon>home</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main class="background">
+      <!--  -->
+      <v-container grid-list-xs>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import cookie from "js-cookie";
+export default {
+  components: {},
+
+  created() {
+    // 如果用户没有登录,重定向至登录界面
+    if (!cookie.get("dhu_token")) {
+      this.$message.warning("请先登录");
+      this.$router.push("/login");
+    } else {
+      this.loginInfo = cookie.getJSON("dhu_ucenter");
+    }
+  },
+  data: () => ({
+    drawer: null,
+    // 用户信息
+    loginInfo: {
+      id: "",
+      age: "",
+      avatar: "",
+      mobile: "",
+      nickname: "",
+      sex: "",
+    },
+    // 左侧抽屉导航栏
+    nav: [
+      {
+        title: "个人中心",
+        route: "/ucenter/info",
+        icon: "person",
+        child: [
+          { title: "我的信息", route: "/ucenter/info", icon: "info" },
+          { title: "修改信息", route: "/ucenter/info/edit", icon: "edit" },
+        ],
+      },
+      {
+        title: "我的消息",
+        route: "/ucenter/message",
+        icon: "mark_email_unread",
+        child: [
+          {
+            title: "查看消息",
+            route: "/ucenter/message",
+            icon: "mark_email_unread",
+          },
+        ],
+      },
+      {
+        title: "我的课程",
+        route: "/ucenter/course",
+        icon: "school",
+        child: [
+          { title: "管理课程", route: "/ucenter/course", icon: "school" },
+        ],
+      },
+      {
+        title: "我的笔记",
+        route: "/ucenter/note",
+        icon: "notes",
+        child: [{ title: "管理笔记", route: "/ucenter/note", icon: "edit" }],
+      },
+      {
+        title: "我的文章",
+        route: "/ucenter/article",
+        icon: "article",
+        child: [
+          { title: "写新文章", route: "/ucenter/article/add", icon: "add" },
+          { title: "管理文章", route: "/ucenter/article", icon: "edit" },
+        ],
+      },
+    ],
+  }),
+};
+</script>
---
+++ pages/ucenter/info/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-breadcrumbs divider="/" :items="breadList"> </v-breadcrumbs>
+    <v-card-title primary-title> 123123 </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  layout: "ucenter",
+  data() {
+    return {
+      breadList: [
+        {
+          disabled: false,
+          // exact: boolean,
+          // href: string,
+          link: true,
+          text: "个人中心",
+        },
+        {
+          disabled: false,
+          // exact: boolean,
+          // href: string,
+          link: true,
+          text: "个人信息",
+          to: "/ucenter/info",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>

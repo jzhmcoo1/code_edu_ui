fc2737325ac99bff695562fe0f67510d0689ab8b
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-22 00:25:04 +0800

    新增视频笔记布局

---
+++ components/DrawerEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-navigation-drawer
+    overlay-color="transparent"
+    right
+    v-model="drawer"
+    bottom
+    app
+    temporary
+    width="400"
+  >
+  </v-navigation-drawer>
+</template>
+<script>
+import PubSub from "pubsub-js";
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  created() {
+    PubSub.subscribe("editDrawer", () => {
+      this.drawer = !this.drawer;
+    });
+  },
+};
+</script>

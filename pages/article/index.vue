0982aae9ed78f1b513361f3a6f6b1a4d2bece41c
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-28 21:29:08 +0800

    文章标签功能完成

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -10,7 +10,7 @@
               <h1 class="headline heading--text">全部文章</h1>
             </v-container>
             <!-- 列表展示区 -->
-            <v-container grid-list-xs>
+            <v-container grid-list-xs v-if="responseData.items.length !== 0">
               <v-row class="mx-auto">
                 <v-col
                   v-for="(item, index) in responseData.items"
@@ -70,6 +70,10 @@
                 </v-col>
               </v-row>
             </v-container>
+            <!-- 无数据提示 -->
+            <v-container grid-list-xs v-else>
+              <a-empty description="这里空空如也" />
+            </v-container>
             <!-- 分页 -->
             <v-container grid-list-xs>
               <div class="text-center">
@@ -89,14 +93,58 @@
       </v-col>
       <!-- 右边栏 -->
       <v-col cols="12" sm="3">
-        <v-row>
+        <!-- <v-row>
           <v-col>
             <v-sheet rounded="lg" min-height="268"> 热门文章 </v-sheet>
           </v-col>
-        </v-row>
+        </v-row> -->
+        <!-- TODO 文章分类 -->
         <v-row style="position: sticky; top: 80px">
           <v-col>
-            <v-sheet rounded="lg" min-height="268"> 文章分类 </v-sheet>
+            <v-sheet rounded="lg" min-height="268">
+              <v-card flat>
+                <v-card-title primary-title> 文章标签 </v-card-title>
+                <v-card-text class="caption"> 一级分类 </v-card-text>
+                <v-card-actions>
+                  <div class="px-4 pb-2">
+                    <v-chip-group
+                      v-model="firstLevelIndex"
+                      active-class="primary--text"
+                      column
+                      @change="searchOne(firstLevelIndex)"
+                    >
+                      <v-chip
+                        filter
+                        v-for="tag in subjectNestedList"
+                        :key="tag.id"
+                      >
+                        {{ tag.title }}
+                      </v-chip>
+                    </v-chip-group>
+                  </div>
+                </v-card-actions>
+                <v-card-text class="caption"> 二级分类 </v-card-text>
+                <v-card-actions>
+                  <!-- 如果二级标签为空则不显示 -->
+                  <div class="px-4 pb-2">
+                    <v-chip-group
+                      v-model="secondLevelIndex"
+                      active-class="primary--text"
+                      column
+                      @change="searchTwo(secondLevelIndex)"
+                    >
+                      <v-chip
+                        filter
+                        v-for="tag in subSubjectList"
+                        :key="tag.id"
+                      >
+                        {{ tag.title }}
+                      </v-chip>
+                    </v-chip-group>
+                  </div>
+                </v-card-actions>
+              </v-card>
+            </v-sheet>
           </v-col>
         </v-row>
       </v-col>
@@ -107,14 +155,13 @@
 <script>
 import FloatBtn from "@/components/FloatBtn.vue";
 import articleApi from "@/api/article";
-import monent, { MomentInput } from "moment";
+import monent from "moment";
 monent.locale("zh-CN");
 export default {
   components: { FloatBtn },
   layout: "article",
   data() {
     return {
-      searchObj: {}, // 查询表单对象
       page: 1, //当前页
       limit: 6, //每页数量
       responseData: {
@@ -142,9 +189,15 @@ export default {
           },
         ],
       },
+      subjectNestedList: [], // 一级分类列表
+      subSubjectList: [], // 二级分类列表
+      searchObj: {}, // 查询表单对象
+      firstLevelIndex: undefined, //实际选中的一级标签
+      secondLevelIndex: undefined, //实际选中的二级标签
     };
   },
   computed: {
+    // 计算高度,给封面图使用
     height() {
       switch (this.$vuetify.breakpoint.name) {
         case "xs":
@@ -159,14 +212,57 @@ export default {
     },
   },
   created() {
-    this.getArticleList();
+    this.getArticleList(); //查询文章列表
+    this.initSubject(); //查询所有一级分类
   },
   methods: {
+    // 点击某个一级分类,查询二级分类
+    // firstLevelIndex为传来的数组下标
+    searchOne(firstLevelIndex) {
+      console.log("选择的firstLevelIndex为", firstLevelIndex);
+      // 如果一级分类不为空
+      if (firstLevelIndex !== undefined) {
+        console.log("一级分类不为空");
+        const firstId = this.subjectNestedList[firstLevelIndex].id; //取得选中的一级标签的标签Id
+        this.searchObj.type = firstId; //将要查询的标签id赋值个searchObj
+        this.subSubjectList = this.subjectNestedList[firstLevelIndex].children; //将选中的一级标签的子标签赋给二级数组
+      } else {
+        console.log("一级分类为空");
+        this.searchObj = {}; //请求searchObj
+        this.subSubjectList = []; //清空二级子标签
+        this.secondLevelIndex = undefined;
+      }
+      console.log("得到的二级分类为", this.subSubjectList);
+      this.getArticleList(); //重新查询
+    },
+    searchTwo(secondLevelIndex) {
+      console.log("选择的secondLevelIndex为", secondLevelIndex);
+      // 如果二级分类不为空
+      if (secondLevelIndex !== undefined) {
+        const secondId = this.subSubjectList[secondLevelIndex].id;
+        this.searchObj.type = secondId;
+      } else {
+        this.searchObj.type = this.subjectNestedList[this.firstLevelIndex].id; //取消选择二级标签后重新赋值一级标签
+        this.subSubjectList = this.subjectNestedList[
+          this.firstLevelIndex
+        ].children;
+      }
+      this.getArticleList(); //重新查询
+    },
+    // 查询文章一级分类
+    initSubject() {
+      articleApi.getAllSubject().then((response) => {
+        this.subjectNestedList = response.data.list;
+        console.log(this.subjectNestedList);
+      });
+    },
+    // 查询文章列表(分页和条件)
     getArticleList() {
       articleApi
         .getArticleList(this.page, this.limit, this.searchObj)
         .then((response) => {
           this.responseData = response.data;
+          console.log(this.responseData);
         });
     },
     // moment格式化时间

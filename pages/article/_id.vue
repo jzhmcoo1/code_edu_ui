458f50559974f8920c5aa6029ab83ca5c69e9ef9
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-28 15:12:56 +0800

    新增文章目录收纳盒

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -1,13 +1,55 @@
 <template>
   <v-container grid-list-xs>
+    <!-- 文章目录收纳盒 -->
+    <v-navigation-drawer app fixed v-model="drawer" class="px-5">
+      <v-sheet rounded="lg" min-height="100vh" class="transparent">
+        <v-card flat class="transparent">
+          <v-card-title primary-title>
+            <v-icon left>toc</v-icon>
+            文章目录
+          </v-card-title>
+          <v-divider></v-divider>
+          <side-catalog
+            v-if="html !== ''"
+            class="catalog pt-2"
+            v-bind="catalogProps"
+          ></side-catalog>
+        </v-card>
+      </v-sheet>
+    </v-navigation-drawer>
+    <!-- 按钮组 -->
+    <v-speed-dial
+      absolute
+      fixed
+      v-model="fab"
+      bottom
+      right
+      direction="top"
+      :open-on-hover="true"
+      transition="slide-y-reverse-transition"
+    >
+      <template v-slot:activator>
+        <v-btn v-model="fab" color="grey darken-2" dark fab>
+          <v-icon v-if="fab"> mdi-close </v-icon>
+          <v-icon v-else> format_list_bulleted </v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="green">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="indigo" @click="drawer = !drawer">
+        <v-icon>toc</v-icon>
+      </v-btn>
+    </v-speed-dial>
+
     <v-row>
       <!-- 左边栏 -->
-      <v-col cols="12" sm="9">
+      <v-col cols="12" md="9">
         <v-sheet min-height="70vh" rounded="lg">
           <v-container grid-list-xs>
             <!-- 文章基本信息 -->
             <v-container grid-list-xs class="text-center">
-              <h1 class="headline heading--text">
+              <h1 class="headline heading--text font-weight-bold">
                 {{ articleInfo.title }}
               </h1>
               <v-card flat>
@@ -15,7 +57,7 @@
                   <v-icon left>mdi-heart</v-icon>
                   {{ articleInfo.likeCount }}
                 </v-btn>
-                <v-btn text>
+                <v-btn text @click="$vuetify.goTo('#comment')">
                   <v-icon left>mdi-comment</v-icon>
                   {{ articleInfo.commentCount }}
                 </v-btn>
@@ -23,8 +65,20 @@
                   <v-icon left>mdi-eye</v-icon>
                   {{ articleInfo.viewCount }}
                 </v-btn>
+                <v-card-text class="caption">
+                  创建时间:
+                  <span class="font-weight-bold"
+                    >{{ formateTime(articleInfo.createTime) }}
+                  </span>
+
+                  更新时间:
+                  <span class="font-weight-bold">
+                    {{ formateTime(articleInfo.modifiedTime) }}
+                  </span>
+                </v-card-text>
               </v-card>
             </v-container>
+            <v-divider></v-divider>
             <!-- 文章内容 -->
             <v-container grid-list-xs>
               <div id="html" ref="html" v-html="html"></div>
@@ -34,7 +88,7 @@
         <!-- 文章评论 -->
         <v-card class="mt-3">
           <v-card-title primary-title>
-            <v-icon>comment</v-icon>
+            <v-icon left>comment</v-icon>
             文章评论
             <v-btn icon @click="showComment = !showComment">
               <v-icon>{{
@@ -43,7 +97,7 @@
             </v-btn>
           </v-card-title>
           <v-expand-transition>
-            <div v-show="showComment">
+            <div id="comment" v-show="showComment">
               <v-divider></v-divider>
               <Comments type="article" :id="articleId" />
             </div>
@@ -51,15 +105,25 @@
         </v-card>
       </v-col>
       <!-- 右边栏 -->
-      <v-col cols="12" sm="3">
-        <v-row>
+      <v-col cols="12" md="3" class="text-center">
+        <v-row style="position: sticky; top: 75px">
           <v-col>
-            <v-sheet rounded="lg" min-height="268"> 作者信息 </v-sheet>
-          </v-col>
-        </v-row>
-        <v-row style="position: sticky; top: 80px">
-          <v-col>
-            <v-sheet rounded="lg" min-height="268"> 文章目录 </v-sheet>
+            <v-sheet rounded="lg">
+              <v-card flat>
+                <v-card-title primary-title>
+                  <v-icon left>person</v-icon>
+                  作者信息
+                </v-card-title>
+                <v-responsive :aspect-ratio="1 / 1">
+                  <v-avatar size="150" class="avatar-rotate">
+                    <img :src="articleInfo.avatar" />
+                  </v-avatar>
+                  <v-card-text>
+                    {{ articleInfo.authorName }}
+                  </v-card-text>
+                </v-responsive>
+              </v-card>
+            </v-sheet>
           </v-col>
         </v-row>
       </v-col>
@@ -72,8 +136,12 @@ import articleApi from "@/api/article";
 import marked from "marked";
 import hljs from "highlight.js";
 import Comment from "@/components/Comments";
+import moment from "moment";
+import SideCatalog from "vue-side-catalog";
+import "vue-side-catalog/lib/vue-side-catalog.css";
+moment.locale("zh-cn");
 export default {
-  components: { Comment },
+  components: { Comment, SideCatalog },
   //params.id获取路径id值
   head() {
     return {
@@ -100,15 +168,26 @@ export default {
   layout: "article",
   data() {
     return {
+      fab: false, //按钮组
+      drawer: true, //目录收纳
       renderer: new marked.Renderer(),
       articleInfo: {
         content: "",
       },
       html: "",
       showComment: true,
+      catalogProps: {
+        container: "#html",
+        levelList: ["h1", "h2", "h3", "h4", "h5"],
+        watch: true,
+      },
     };
   },
   methods: {
+    formateTime(time) {
+      return moment(time).fromNow();
+    },
+    // 初始化文章信息
     initArticle() {
       articleApi.getArticleInfo(this.articleId).then((response) => {
         this.articleInfo = response.data.article;
@@ -117,12 +196,19 @@ export default {
           heading(text, level) {
             const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
             return `
-            <h${level} class="heading--text">
+            <h${level} class="heading--text my-2 ${
+              level === 1 ? "text-center" : ""
+            }">
               <a name="${escapedText}" class="anchor" href="#${escapedText}">
                 <span class="header-link"></span>
               </a>
               ${text}
             </h${level}>`;
+          },
+          paragraph(text) {
+            return `
+            <p class="body-1">${text}</p>
+            `;
           },
         };
         marked.use({ renderer });

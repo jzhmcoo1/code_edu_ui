832323946e7a254c43dfc5addb5a690660b75c6c
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-04-30 15:11:38 +0800

    更新底部栏

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,18 +2,18 @@
   <v-card>
     <v-footer v-bind="localAttrs" :padless="padless">
       <v-card flat tile width="100%" class="primary lighten-1 text-center">
-        <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-
         <v-divider></v-divider>
 
         <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          {{ new Date().getFullYear() }} —
+          <strong>
+            <a
+              class="white--text"
+              href="https://github.com/jzhmcoo1/code_edu_ui"
+            >
+              CodeEdu
+            </a>
+          </strong>
         </v-card-text>
       </v-card>
     </v-footer>
@@ -23,8 +23,6 @@
 <script>
 export default {
   data: () => ({
-    icons: ["mdi-home", "mdi-email", "mdi-calendar", "mdi-delete"],
-    items: ["default", "absolute", "fixed"],
     padless: true,
     variant: "default",
   }),

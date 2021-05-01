b6a88c2d870630d240e259152b5981e6e63175ef
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-01 16:39:26 +0800

    引入退选课api

--- api/ucenter.ts
+++ api/ucenter.ts
@@ -7,5 +7,28 @@ export default {
       method: 'post',
       data: ucenter
     })
+  },
+  // 获取已登录用户的课程
+  getMyCourseList(page: number, limit: number, ucenterId: string) {
+    return service({
+      url: `/eduService/edu-mycourse/pageMyCourese/${page}/${limit}`,
+      method: 'get',
+      params: { ucenterId }
+    })
+  },
+  // 添加一门课程(选课)
+  addMyCourse(mycourse: object) {
+    return service({
+      url: `/eduService/edu-mycourse/auth/save`,
+      method: 'post',
+      data: mycourse
+    })
+  },
+  // 删除一门课程(退课)
+  deletdMyCourese(courseId: string) {
+    return service({
+      url: `/eduService/edu-mycourse/deleteMyCourse/${courseId}`,
+      method: 'delete'
+    })
   }
 }

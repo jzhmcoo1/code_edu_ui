22757858eb08164e816547def3a3cb72468d6e06
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-14 15:55:31 +0800

    修改antd的按需引入

--- plugins/ant-design-vue.js
+++ plugins/ant-design-vue.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-import { message, Empty } from 'ant-design-vue';
+import { message, Empty, notification } from 'ant-design-vue';
 message.config({
   top: `100px`,
-  duration: 2,
   maxCount: 3,
 });
 Vue.config.productionTip = false;
 Vue.prototype.$message = message;
 Vue.use(Empty)
+Vue.use(notification)

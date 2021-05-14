7fabc840256f3f8db7ec61b4cc73d27d6a2475ef
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-14 15:54:27 +0800

    修改login获取后端验证码图片

--- api/login.ts
+++ api/login.ts
@@ -1,54 +1,13 @@
 import { service } from '@/utils/request'
-
 export default {
-  //登录的方法
-  submitLoginUser(userInfo: object) {
+  // 获取图像验证码
+  getCodeImage(randomId: string) {
     return service({
-      url: `/educenter/member/login`,
-      method: 'post',
-      data: userInfo
+      method: 'get',
+      url: `/auth/captcha`,
+      responseType: 'arraybuffer',
+      params: { key: randomId }
     })
   },
 
-  //根据token获取用户信息
-  getLoginUserInfo() {
-    return service({
-      url: `/educenter/member/getMemberInfo`,
-      method: 'get'
-    })
-  },
-
-  //忘记密码
-  forgetPassword(userPwd: string) {
-    return service({
-      url: `/educenter/member/forgetPassword`,
-      method: 'post',
-      data: userPwd
-    })
-  },
-
-  //修改密码
-  changePassword(newPwd: string) {
-    return service({
-      url: `/educenter/member/changePassword`,
-      method: 'post',
-      data: newPwd
-    })
-  },
-
-  // 检查手机号码是否存在
-  checkMobile(mobile: string) {
-    return service({
-      url: `/educenter/member/checkMobile`,
-      method: 'post',
-      params: mobile
-    })
-  },
-  updateUcenter(ucenter: string) {
-    return service({
-      url: `/educenter/member/updateUcenter`,
-      method: 'post',
-      data: ucenter
-    })
-  }
 }

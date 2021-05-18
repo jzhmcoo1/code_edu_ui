ca16bac78af49920cea330ab671eb29f2b226b67
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-18 11:44:42 +0800

    修复了路由守卫失效

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 import { message } from 'ant-design-vue'
 export default function ({ redirect, store }) {
-  if (store.state.account.user === undefined || store.state.account.user.userId === "") {
+  console.log(store.state.account)
+  if (store.state.account.user === undefined || store.state.account.user.userId === undefined || store.state.account.user.userId === "") {
     message.warning("请先登录")
     return redirect('/login');
   }

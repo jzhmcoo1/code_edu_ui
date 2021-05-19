248ca407588798fd7843b4b714956c3b10bb04eb
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-19 18:16:06 +0800

    删除了输出内容

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 import { message } from 'ant-design-vue'
 export default function ({ redirect, store }) {
-  console.log(store.state.account)
   if (store.state.account.user === undefined || store.state.account.user.userId === undefined || store.state.account.user.userId === "") {
     message.warning("请先登录")
     return redirect('/login');

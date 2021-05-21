28a4414b8ac5693a1e676e7a8d4a3e548d965ee8
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-21 14:55:57 +0800

    前台笔记页面完成

--- components/DrawerEditor.vue
+++ components/DrawerEditor.vue
@@ -9,16 +9,18 @@
     width="400"
   >
     <v-container grid-list-xs>
-      <v-row>
-        <v-col cols="12" class="d-flex justify-space-around">
-          <v-btn @click="addNote" class="primary">
-            新建笔记
-            <v-icon right>mdi-plus-circle</v-icon>
+      <v-row justify="center">
+        <v-col>
+          <v-btn block @click="saveNote" color="primary">
+            {{ note.id === "" ? "添加" : "保存" }}笔记
+            <v-icon right>mdi-checkbox-marked-circle</v-icon>
           </v-btn>
+        </v-col>
+        <v-col>
           <v-dialog v-model="dialog" persistent max-width="290">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn class="error" dark v-bind="attrs" v-on="on">
-                删除笔记
+              <v-btn block color="error" v-bind="attrs" v-on="on">
+                {{ note.id === "" ? "清空" : "删除" }}笔记
                 <v-icon right>mdi-delete-circle</v-icon>
               </v-btn>
             </template>
@@ -36,15 +38,19 @@
               </v-card-actions>
             </v-card>
           </v-dialog>
-          <v-btn @click="saveNote" class="success">
-            保存笔记
-            <v-icon right>mdi-checkbox-marked-circle</v-icon>
-          </v-btn>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-sheet class="white">
+            <v-text-field
+              outlined
+              dense
+              clearable
+              name="title"
+              label="笔记标题"
+              v-model="note.title"
+            ></v-text-field>
             <client-only>
               <editor ref="toastuiEditor" previewStyle="tab" height="500px" />
             </client-only>
@@ -57,7 +63,9 @@
 </template>
 <script>
 import PubSub from "pubsub-js";
+import noteApi from "~/api/note";
 export default {
+  middleware: "auth",
   data: () => ({
     drawer: false,
     group: null,
@@ -65,28 +73,95 @@ export default {
       hideModeSwitch: true,
     },
     dialog: false, //删除确认提示框
+    note: {
+      authorId: "",
+      content: "",
+      createTime: "",
+      id: "",
+      modifiedTime: "",
+      title: "",
+      videoId: "",
+    },
   }),
 
   created() {
     PubSub.subscribe("editDrawer", () => {
-      this.drawer = !this.drawer;
+      this.drawer = !this.drawer; //组件收纳和打开
     });
+    // 将作者id和videoId进行初始化
+    this.note.authorId = this.$store.state.account.user.userId;
+    this.note.videoId = this.$route.query.videoId;
+    this.checkExist();
+  },
+  // 监听路由videoId变化
+  watch: {
+    $route() {
+      this.note.videoId = this.$route.query.videoId;
+      this.checkExist();
+    },
   },
   methods: {
+    setMarkdown() {
+      return this.$refs.toastuiEditor.invoke("setMarkdown", this.note.content);
+    },
     getHtml() {
       return this.$refs.toastuiEditor.invoke("getHtml");
     },
-    addNote() {
-      // 保存当前笔记
-      // 清空当前内容
+    getMarkdown() {
+      return this.$refs.toastuiEditor.invoke("getMarkdown");
     },
-    deleteNote() {
-      // 调用reset接口,清空当前编辑器
-      this.$refs.toastuiEditor.invoke("reset");
+    // 检查该视频下是否存在视频,如果存在,则取回数据填入内容中
+    async checkExist() {
+      const exist = await noteApi.checkExistNote(this.note.videoId);
+      if (exist.data.noteId !== null) {
+        this.note.id = exist.data.noteId;
+        const response = await noteApi.detailNote(this.note.id);
+        console.log(response);
+        this.note.title = response.data.note.title;
+        this.note.content = response.data.note.content;
+        this.setMarkdown();
+      } else {
+        this.$refs.toastuiEditor.invoke("reset");
+        this.note.id = "";
+        this.note.title = "";
+        this.note.content = "";
+      }
+    },
+    async deleteNote() {
+      // 如果笔记id不为空,表示数据库存在这篇笔记
+      if (this.note.id !== "") {
+        const deleted = await noteApi.deleteNote(this.note.id);
+        if (deleted.code === 200) {
+          this.$message.success("删除笔记成功😁");
+          this.note.id = "";
+        }
+      } else {
+        // 如果id为空,则笔记只保存在本地,清空本地即可
+        this.$message.success("清空笔记成功🙅");
+      }
+      this.$refs.toastuiEditor.invoke("reset"); // 调用reset接口,清空当前编辑器
+      // 清空本地的数据
+      this.note.content = "";
+      this.note.title = "";
       this.dialog = false;
-      this.$message.success("删除笔记成功");
     },
-    saveNote() {},
+    async saveNote() {
+      // 调用toastEditor接口,获取markdown内容
+      this.note.content = this.getMarkdown();
+      let saved;
+      if (this.note.id === "") {
+        // id为空则添加
+        saved = await noteApi.addNote(this.note);
+      } else {
+        // id不为空则更新
+        saved = await noteApi.updateNote(this.note);
+      }
+      if (saved.code === 200) {
+        this.$message.success("笔记保存成功");
+      }
+      // 添加/保存完成后重新获取内容
+      this.checkExist();
+    },
   },
 };
 </script>

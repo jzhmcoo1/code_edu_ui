0d4ef815d3eb31030bbd8ca37c2ba43d7fb0488e
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-05-28 18:08:05 +0800

    新增搜索接口

---
+++ api/search.ts
@@ -0,0 +1,32 @@
+import { service } from '@/utils/request';
+const api_prefix = "/service/search"
+export default {
+  /**
+   * 获取搜索建议
+   * @param keyword 关键字
+   * @param type 类型
+   * @returns 
+   */
+  suggest(keyword: string) {
+    return service({
+      url: `${api_prefix}/suggest`,
+      params: { keyword },
+      method: 'get'
+    })
+  },
+
+  /**
+   * 获取搜索结果
+   * @param page 页数
+   * @param limit 每页个数
+   * @param keyword 关键词
+   * @returns 
+   */
+  result(page: number, limit: number, keyword: string) {
+    return service({
+      url: `${api_prefix}/result`,
+      method: 'get',
+      params: { page, limit, keyword }
+    })
+  }
+}

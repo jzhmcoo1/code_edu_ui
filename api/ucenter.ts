bac8d3f5b418f96f8842a528d97d702c9b67eb94
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-06-13 17:21:51 +0800

    引入用户选课api

--- api/ucenter.ts
+++ api/ucenter.ts
@@ -1,25 +1,23 @@
 import { service } from "@/utils/request";
 export default {
   // 获取已登录用户的课程
-  getMyCourseList(page: number, limit: number, ucenterId: string) {
+  getMyCourseList(page: number, limit: number) {
     return service({
-      url: `/eduService/edu-mycourse/pageMyCourese/${page}/${limit}`,
+      url: `/service/selection/${page}/${limit}`,
       method: 'get',
-      params: { ucenterId }
     })
   },
   // 添加一门课程(选课)
-  addMyCourse(mycourse: object) {
+  addMyCourse(courseId: string) {
     return service({
-      url: `/eduService/edu-mycourse/auth/save`,
+      url: `/service/selection/save/${courseId}`,
       method: 'post',
-      data: mycourse
     })
   },
   // 删除一门课程(退课)
   deletdMyCourese(courseId: string) {
     return service({
-      url: `/eduService/edu-mycourse/deleteMyCourse/${courseId}`,
+      url: `/service/selection/${courseId}`,
       method: 'delete'
     })
   }

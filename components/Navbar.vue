ca7d20973e041995cc32910e7789f00c36b2e94f
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-06-15 11:14:48 +0800

    添加未读消息badge

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -48,15 +48,22 @@
             <v-menu open-on-hover bottom offset-y>
               <template v-slot:activator="{ on, attrs }">
                 <!-- 展示用户头像 -->
-                <v-avatar
-                  class="mx-2 avatar-rotate"
-                  color="grey darken-1"
-                  size="32"
-                  v-bind="attrs"
-                  v-on="on"
+                <v-badge
+                  :content="messages"
+                  :value="messages"
+                  color="info"
+                  overlap
                 >
-                  <v-img :src="loginInfo.avatar"></v-img>
-                </v-avatar>
+                  <v-avatar
+                    class="mx-2 avatar-rotate"
+                    color="grey darken-1"
+                    size="32"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    <v-img :src="loginInfo.avatar"></v-img>
+                  </v-avatar>
+                </v-badge>
               </template>
               <v-list dense>
                 <v-list-item
@@ -66,7 +73,7 @@
                   :key="index"
                 >
                   <v-list-item-title>
-                    <v-icon left>home</v-icon>
+                    <v-icon left>{{ item.icon }}</v-icon>
                     {{ item.title }}
                   </v-list-item-title>
                 </v-list-item>
@@ -75,6 +82,7 @@
             <v-btn @click="logout" icon plain color="primary">
               <v-icon>logout</v-icon>
             </v-btn>
+            <Message v-show="false" />
           </div>
         </client-only>
       </v-container>
@@ -82,14 +90,17 @@
   </v-app-bar>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import SearchBar from "./SearchBar.vue";
-export default Vue.extend({
+<script>
+import SearchBar from "./SearchBar";
+import messageApi from "@/api/message";
+import Message from "./Messge.vue";
+export default {
   components: {
     SearchBar,
+    Message,
   },
   data: () => ({
+    websocket: WebSocket,
     links: [
       { title: "首页", route: "/", icon: "home" },
       { title: "课程", route: "/course", icon: "school" },
@@ -106,23 +117,73 @@ export default Vue.extend({
       username: "",
       sex: "",
     },
-    menus: [{ title: "个人中心", icon: "home", route: "/ucenter/info" }],
+    menus: [
+      { title: "个人中心", icon: "home", route: "/ucenter/info" },
+      { title: "消息中心", icon: "email", route: "/ucenter/message" },
+      { title: "我的课程", icon: "school", route: "/ucenter/course" },
+      { title: "我的文章", icon: "article", route: "/ucenter/article" },
+      { title: "我的笔记", icon: "edit", route: "/ucenter/note" },
+    ],
+    messages: 0,
+    unreadList: [],
   }),
   created() {
+    this.connectWebSocket();
     this.showInfo();
   },
   methods: {
+    connectWebSocket() {
+      if (
+        "WebSocket" in window &&
+        this.$store.state.account.user !== undefined
+      ) {
+        this.websocket = new WebSocket(
+          `ws://1.15.24.22:8301/service/websocket/${this.$store.state.account.user.userId}`
+        );
+        this.initWebSocket();
+      } else {
+        console.warn("浏览器不支持WebSocket或用户未登录");
+      }
+    },
+    initWebSocket() {
+      // 连接错误
+      this.websocket.onerror = () => {
+        console.error(
+          "WebSocket连接发生错误 状态码：" + this.websocket.readyState
+        );
+      };
+      // 连接成功
+      this.websocket.onopen = () => {
+        console.log("WebSocket连接成功 状态码：" + this.websocket.readyState);
+      };
+      // 收到消息的回调
+      this.websocket.onmessage = (event) => {
+        // 根据服务器推送的消息做自己的业务处理
+        console.log("服务端返回：" + event.data);
+        this.checkUnreadMessages();
+      };
+      // 连接关闭的回调
+      this.websocket.onclose = () => {
+        console.log("WebSocket连接关闭 状态码：" + this.websocket.readyState);
+      };
+      // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+      window.onbeforeunload = () => {
+        this.websocket.close();
+      };
+    },
     // 如果用户登录,则展示用户信息
     showInfo() {
       const userInfo = this.$store.state.account.user;
       if (userInfo.userId !== undefined) {
         this.loginInfo = userInfo;
+        this.checkUnreadMessages();
       } else {
         this.loginInfo.userId = "";
       }
     },
     // 用户退出登录
     logout() {
+      this.websocket.close();
       this.loginInfo.userId = "";
       this.$store.commit("account/removeAll");
       this.$message.success({
@@ -132,6 +193,13 @@ export default Vue.extend({
         this.$router.replace("/");
       }
     },
+    // 检查未读消息
+    checkUnreadMessages() {
+      messageApi.checkUnreadCount().then((response) => {
+        this.messages = response.data.count;
+        this.unreadList = response.data.unReadMessage;
+      });
+    },
   },
-});
+};
 </script>

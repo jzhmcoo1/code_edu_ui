ab31e364e4d4de14b374ab3a3023b393481cd1b2
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-06-15 11:15:21 +0800

    新增支持的message类型

--- api/schema/message.ts
+++ api/schema/message.ts
@@ -7,5 +7,12 @@ export interface EduMessage {
   memberId: string//	接受消息成员id
   modifiedTime: string	//更新时间
   senderName: string//	发送者昵称
+  /**
+   * 0 表示 comment
+   * 1 表示 commentReply
+   * 2 表示 点赞
+   * 3 表示 课程更新
+   * 4 表示 考试发布
+   */
   type: number //类型
 }

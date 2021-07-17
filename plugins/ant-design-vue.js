45897eef5407ce4df40aaa2d896627550b296c4a
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-07-17 17:28:48 +0800

    修复notification没有绑定到Vue原型的问题

--- plugins/ant-design-vue.js
+++ plugins/ant-design-vue.js
@@ -4,7 +4,11 @@ message.config({
   top: `100px`,
   maxCount: 3,
 });
+notification.config({
+  duration: 3
+})
 Vue.config.productionTip = false;
 Vue.prototype.$message = message;
+Vue.prototype.$notification = notification
 Vue.use(Empty)
 Vue.use(notification)

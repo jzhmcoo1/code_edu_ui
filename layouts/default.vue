b487d36f652219d484c5308bac84fde686470d9b
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-07-19 12:58:33 +0800

    删除冗余结构

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,27 +56,6 @@
           <nuxt keep-alive />
         </v-sheet>
       </v-container>
-      <v-container>
-        <v-row>
-          <!-- <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-
-            </v-sheet>
-          </v-col> -->
-
-          <!-- <v-col cols="24" sm="8">
-            <v-sheet min-height="100vh" rounded="lg">
-              <nuxt />
-            </v-sheet>
-          </v-col> -->
-
-          <!-- <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-
-            </v-sheet>
-          </v-col> -->
-        </v-row>
-      </v-container>
     </v-main>
     <Footer />
   </v-app>

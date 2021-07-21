1a3986d30359b2aa951c6d76f7fc42651fba629a
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-07-21 22:57:59 +0800

    初始化考试记录详情页todo

---
+++ pages/ucenter/exam/_id.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout row wrap> </v-layout>
+</template>
+
+<script>
+import examApi from "@/api/exam";
+export default {
+  layout: "ucenter",
+  data() {
+    return {
+      answersMap: {},
+      answersRightMap: {},
+      resultMap: {},
+      examRecord: {},
+    };
+  },
+  created() {
+    this.getRecordById();
+  },
+  methods: {
+    getRecordById() {
+      examApi.recordDetail(this.$route.params.id).then((response) => {
+        console.log(response.data);
+        this.answersMap = response.data.answersMap;
+        this.answersRightMap = response.data.answersRightMap;
+        this.resultMap = response.data.resultMap;
+        this.examRecord = response.data.examRecord;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
